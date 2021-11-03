56ee7bccb3096654fe8dbb9f283d604fed3bfbaf
XamDor <xamdor@protonmail.com>
2021-11-03 15:47:41 +0100

    v1.5.0, (avec erreur)
    -mise en place des objets en cours
    -le haut et le bas de la maison sont prés et la liaison marche
    -les murs et portes on été mit
    
    -les réponses ne sont pas implémenter completement

--- aventureTextuel_21_10_2021.py
+++ aventureTextuel_21_10_2021.py
@@ -1,54 +1,112 @@
 import json
+import pygame
+
 
 class Salle:
     def __init__(self):
         #initialisation des json
         #initialisation des map(maison,monde,magasin)
+        
         self.x=0
-        self.y=2
+        self.y=3
+
+        self.lieu_name="maisonR"
         with open('map.json') as f:
             self.map = json.load(f)
         with open('salle.json') as f:
-            self.salle = json.load(f)
-    def get_salle(self, x,y):
-        #retourne l'ID de la salle de coordoné x,y
+            self.salle_dict = json.load(f)
+        with open('element2jeu.json') as f:    
+            self.element2jeu = json.load(f)
+    def get_map(self, x,y, name_lieu_get_map, name_map_get_map):
+        #retourne l'ID de coordoné x,y
         try:
-            return self.map[y][x]
+            return self.map[name_lieu_get_map][name_map_get_map][y][x]
         except:
             return 0
+
+    def get_salle(self, x,y, name_lieu_get_salle):
+        #retourne l'ID de la salle de coordoné x,y
+        try:
+            return self.map[name_lieu_get_salle]["salle"][y][x]
+        except:
+            return 0
+    
+    def get_mur(self, x,y, name_lieu_get_mur):
+        #retourne les portes qui touchent la salle
+        #dans l'ordre: [z,s,q,d] ou [haut, bas, gauche, droite]
+        rep_get_mur={}
+        try:
+            rep_get_mur["h"]=self.map[name_lieu_get_mur]["mur"][((y-1)*2)+1][x]
+            rep_get_mur["b"]=self.map[name_lieu_get_mur]["mur"][(y*2)+1][x]
+            rep_get_mur["g"]=self.map[name_lieu_get_mur]["mur"][y*2][x]
+            rep_get_mur["d"]=self.map[name_lieu_get_mur]["mur"][y*2][x+1]
+            #on cherche toute les direction
+            for i in rep_get_mur:
+                #pour chaque direction on stoke le contenu (mur,porte,objet) dans "contenui"
+                contenui=rep_get_mur[i]
+                #on stoke la condition pour avancer (none=>on ne peut pas, false=> dans toute condition,"objet"=>on peut si on a l'objet)
+                condition_get_mur=self.element2jeu["deplacement"][contenui]["condition"]
+                
+                if condition_get_mur==False:
+                    rep_get_mur[i]=True
+                elif condition_get_mur in self.inventaire:
+                    rep_get_mur[i]=False
+                elif condition_get_mur:
+                    rep_get_mur[i]=False
+                    #on explique la raison du refu du deplacent
+                    rep_get_mur["erreur"]=self.element2jeu["deplacement"][i]["erreur"] 
+        except:
+            rep_get_mur["erreur"]="erreur exept in Salle.get_mur()"
+        return rep_get_mur
+        
     def action(self, entrer):
         #menu de la salle
         #les fleches pour changer de salle
         #[0] pour rechercher
         #[1:9] pour interagir
-        if entrer=="z":
+        mur=self.get_mur(self.x,self.y,self.lieu_name)
+        print("mur : ", mur)
+        if entrer=="z" and mur["h"]==True:
                 self.y+=-1
-                return "y",-1
-        elif entrer=="s":
+        elif entrer=="s" and mur["b"]==True:
                 self.y+=1
-                return "y",1
-        elif entrer=="q":
+        elif entrer=="q" and mur["g"]==True:
                 self.x+=-1
-                return "x",-1
-        elif entrer=="d":
+        elif entrer=="d" and mur["d"]==True:
                 self.x+=1
-                return "x",1
         elif entrer=="e":
             exit()
-        
+        else:
+            #on affiche la raison de l'impossibiliter du déplacement
+            print(mur["erreur"])
+            return 0
+        #salle_action contient l'ID de la salle 
+        salle_action = self.map[self.lieu_name]["salle"][self.y][self.x]
+        lieu_name_action=self.lieu_name
+        #si la salle avec cette ID est "join" (mene vers un autre lieu)
+        if self.salle_dict[self.lieu_name][salle_action]["join"]:
+            #on change le lieu par le lieu stoké dans le "join"
+            self.lieu_name=self.salle_dict[self.lieu_name][salle_action]["join"]["lieu"] 
+            # on modifi x et y à partir de la salle d'arricver contenut dans "join" et on récupere dans cette salle la la "pos"
+            self.x=self.salle_dict[self.lieu_name][self.salle_dict[lieu_name_action][salle_action]["join"]["salle"]]["pos"]["x"]
+            self.y=self.salle_dict[self.lieu_name][self.salle_dict[lieu_name_action][salle_action]["join"]["salle"]]["pos"]["y"]   
     def annonce(self):
-        act=input("action :  ")
-        pos,signe=self.action(act)
-        if self.get_salle(self.x,self.y)==0:
-            if pos=="y":
-                self.y-=signe
-            elif pos=="x":
-                self.y-=signe
-        print(self.salle["salle"][self.map[self.y][self.x]]["name"])
+        print(self.salle_dict[self.lieu_name][self.map[self.lieu_name]["salle"][self.y][self.x]]["name"])
 
+debug=None
 if __name__=="__main__":
     s=Salle()
     print(s.map)
     while True:
+        act=input("action :  ")
+        s.action(act)
         s.annonce()
+        y=s.y
+        if debug:
+            print("x :",s.x,"\ny : ", y)
+            print("mur y :\n h :",((y-1)*2)+1)
+            print("s :",(y*2)+1)
+            print("g :",y*2)
+            print("d :",y*2)
+
 
