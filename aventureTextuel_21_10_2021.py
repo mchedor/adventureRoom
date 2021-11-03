bb2a671895212965f86f3c7368d97b9989f425db
XamDor <xamdor@protonmail.com>
2021-11-03 17:06:53 +0100

    v1.5.2
    les murs, porte et clé sont opérationnels

--- aventureTextuel_21_10_2021.py
+++ aventureTextuel_21_10_2021.py
@@ -41,6 +41,7 @@ class Salle:
             rep_get_mur["b"]=self.map[name_lieu_get_mur]["mur"][(y*2)+1][x]
             rep_get_mur["g"]=self.map[name_lieu_get_mur]["mur"][y*2][x]
             rep_get_mur["d"]=self.map[name_lieu_get_mur]["mur"][y*2][x+1]
+            erreur_action=None
             #on cherche toute les direction
             for i in rep_get_mur:
                 #pour chaque direction on stoke le contenu (mur,porte,objet) dans "contenui"
@@ -51,13 +52,18 @@ class Salle:
                 if condition_get_mur==False:
                     rep_get_mur[i]=True
                 elif condition_get_mur in self.inventaire:
-                    rep_get_mur[i]=False
+                    rep_get_mur[i]=True
                 elif condition_get_mur:
                     rep_get_mur[i]=False
                     #on explique la raison du refu du deplacent
-                    rep_get_mur["erreur"]=self.element2jeu["deplacement"][i]["erreur"] 
-        except :
-            rep_get_mur["erreur"]="erreur exept in Salle.get_mur()"
+                    erreur_action=self.element2jeu["deplacement"][contenui]["erreur"]
+                else:
+                    rep_get_mur[i]=False 
+            if erreur_action:
+                rep_get_mur["erreur"]=erreur_action
+        except ValueError as a:
+            rep_get_mur["erreur"]=a#"erreur exept in Salle.get_mur()"
+        
         return rep_get_mur
         
     def action(self, entrer):
@@ -67,6 +73,7 @@ class Salle:
         #[1:9] pour interagir
         mur=self.get_mur(self.x,self.y,self.lieu_name)
         print("mur : ", mur)
+        print("inv : ",self.inventaire)
         if entrer=="z" and mur["h"]==True:
                 self.y+=-1
         elif entrer=="s" and mur["b"]==True:
@@ -81,16 +88,30 @@ class Salle:
             #on affiche la raison de l'impossibiliter du déplacement
             print(mur["erreur"])
             return 0
+        
+        #ON VERRIFIE SI LA SALLE EST JOIN
         #salle_action contient l'ID de la salle 
         salle_action = self.map[self.lieu_name]["salle"][self.y][self.x]
+        #lieu_name_action est une variable de passage contenant le nom du premier lieu.
         lieu_name_action=self.lieu_name
         #si la salle avec cette ID est "join" (mene vers un autre lieu)
         if self.salle_dict[self.lieu_name][salle_action]["join"]:
             #on change le lieu par le lieu stoké dans le "join"
             self.lieu_name=self.salle_dict[self.lieu_name][salle_action]["join"]["lieu"] 
-            # on modifi x et y à partir de la salle d'arricver contenut dans "join" et on récupere dans cette salle la la "pos"
+            # on modifie x et y à partir de la salle d'arriver contenut dans "join" et on récupere dans cette salle la la "pos"
             self.x=self.salle_dict[self.lieu_name][self.salle_dict[lieu_name_action][salle_action]["join"]["salle"]]["pos"]["x"]
             self.y=self.salle_dict[self.lieu_name][self.salle_dict[lieu_name_action][salle_action]["join"]["salle"]]["pos"]["y"]   
+        
+        #ON VERRIFIE SI LA SALLE CONTIENT UN OBJET
+        #salle_action contient l'ID de la salle 
+        salle_action = self.map[self.lieu_name]["salle"][self.y][self.x]
+        #salle_action contient la salle 
+        salle_action = self.salle_dict[self.lieu_name][salle_action]
+        if "object" in salle_action:
+            if self.element2jeu["object"][salle_action["object"]]["name"] not in self.inventaire:
+                self.inventaire.append(self.element2jeu["object"][salle_action["object"]]["name"])
+            
+
     def annonce(self):
         print(self.salle_dict[self.lieu_name][self.map[self.lieu_name]["salle"][self.y][self.x]]["name"])
 
